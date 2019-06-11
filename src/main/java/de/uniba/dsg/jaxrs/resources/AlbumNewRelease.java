1393401fa0631591cbba95f6ae4a46fa8c90f8e3
Mohammad Sazibul Islam <xibic@warptec.com>
2019-06-12 01:43:42 +0200

    Unused import and code removed

--- src/main/java/de/uniba/dsg/jaxrs/resources/AlbumNewRelease.java
+++ src/main/java/de/uniba/dsg/jaxrs/resources/AlbumNewRelease.java
@@ -3,7 +3,6 @@ package de.uniba.dsg.jaxrs.resources;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
--- src/main/java/de/uniba/dsg/jaxws/resources/AlbumNewRelease.java
+++ src/main/java/de/uniba/dsg/jaxws/resources/AlbumNewRelease.java
@@ -1,33 +1,12 @@
 package de.uniba.dsg.jaxws.resources;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-
-import org.glassfish.jersey.server.internal.scanning.ResourceFinderException;
-
-import com.neovisionaries.i18n.CountryCode;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
-import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
-import com.wrapper.spotify.model_objects.specification.Paging;
-import com.wrapper.spotify.requests.data.browse.GetListOfNewReleasesRequest;
-
-import de.uniba.dsg.CustomSpotifyApi;
 import de.uniba.dsg.interfaces.AlbumApi;
-import de.uniba.dsg.jaxrs.exceptions.ClientRequestException;
-import de.uniba.dsg.jaxrs.exceptions.RemoteApiException;
 import de.uniba.dsg.jaxws.MusicApiImpl;
 import de.uniba.dsg.jaxws.exceptions.MusicRecommenderFault;
 import de.uniba.dsg.models.ErrorMessage;
 import de.uniba.dsg.models.Release;
-import de.uniba.dsg.models.Song;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
@@ -35,8 +14,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class AlbumNewRelease implements AlbumApi {
-
-	private static final Logger logger = Logger.getLogger(AlbumNewRelease.class.getName());
 	
 	@Override
     public List<Release> getNewReleases(String country, int size) {
