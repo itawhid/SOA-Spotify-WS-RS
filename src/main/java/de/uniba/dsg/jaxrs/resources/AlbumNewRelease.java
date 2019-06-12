eb54bbd028e5a9bbc1d83d8c3c55cce67fea4a79
Mohammad Sazibul Islam <xibic@warptec.com>
2019-06-12 20:25:07 +0200

    Updated: handling bad request and exceptions.

--- src/main/java/de/uniba/dsg/jaxrs/resources/AlbumNewRelease.java
+++ src/main/java/de/uniba/dsg/jaxrs/resources/AlbumNewRelease.java
@@ -32,21 +32,33 @@ public class AlbumNewRelease implements AlbumApi {
 	@GET
 	public List<Release> getNewReleases(@QueryParam("country") String country, @QueryParam("size") int size) throws ClientRequestException {
 		
-		/**
-		 * country = list of new releases from a specific country, empty is accepted
-		 */
 		CountryCode countryCode = null;
+		//
+		GetListOfNewReleasesRequest listOfNewAlbumReleasesRequest = null;
+		//Result set
+		List<Release> latestReleaseList = new ArrayList<>();
+		
+		
+		/**
+		 * Check for Invalid Size
+		 * API requirement: size between 01 and 50
+		 */
+		if(size < 0 || size > 50) {
+			throw new ClientRequestException(new ErrorMessage(" Bad Request: Invalid Size(should be between 01 and 50) "));
+		}
+		
+		/**
+		 * Check for Invalid Country code
+		 */
 		if(country != null) {
-			//Ignore cases for Country
+			//Ignore upper/lower cases for Country string
 			countryCode = CountryCode.getByCodeIgnoreCase(country);
 			if(countryCode == null) {
-				throw new ClientRequestException(new ErrorMessage("No valid country found with specified value"));
+				throw new ClientRequestException(new ErrorMessage(" Bad Request: Invalid Country Code "));
 			}
 		}
-
+		
 		//Request builder based on parameters
-		GetListOfNewReleasesRequest listOfNewAlbumReleasesRequest = null;
-
 		if(country == null && size == 0) {
 			listOfNewAlbumReleasesRequest = CustomSpotifyApi.getInstance().getListOfNewReleases().build();
 		}else if(country == null && size > 0) {
@@ -57,14 +69,13 @@ public class AlbumNewRelease implements AlbumApi {
 			listOfNewAlbumReleasesRequest = CustomSpotifyApi.getInstance().getListOfNewReleases().country(countryCode).limit(size).build();
 		}
 
-		//Returned list from response
-		List<Release> latestAlbumList = new ArrayList<>();
 
+		//Request
 		try {
 
-			//Response 
 			Paging<AlbumSimplified> albumListResponse = listOfNewAlbumReleasesRequest.execute();
-
+			
+			//Response check
 			if(albumListResponse.getTotal() > 0) {
 				//Add album items from the response to List
 				AlbumSimplified[] albumSimplified = albumListResponse.getItems();
@@ -86,19 +97,19 @@ public class AlbumNewRelease implements AlbumApi {
 
 					release.setArtist(artistName);
 
-					latestAlbumList.add(release);
+					latestReleaseList.add(release);
 				}
 			}
 			else {
-				throw new ResourceFinderException("No latest release for the specified country");
+				throw new ResourceFinderException("No Result found for the specified request");
 			}
 		} catch (SpotifyWebApiException e) {
-			throw new RemoteApiException(new ErrorMessage("Sorry,Could not find any new album for the specified country"));
+			throw new RemoteApiException(new ErrorMessage("No Result found for the specified request"));
 		} catch (IOException e) {
-			throw new ClientRequestException(new ErrorMessage("Some network error occurred while requesting"));
+			throw new ClientRequestException(new ErrorMessage("400 : Bad Request"));
 		}
         //Return result album list
-		return latestAlbumList;
+		return latestReleaseList;
 	}
 
 }
