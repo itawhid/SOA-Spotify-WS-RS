09e8bddffd8cc59bddae96d1b3940d8aed1c38ca
Md Abdullah Al Mahmud <md-abdullah-al.mahmud@stud.uni-bamberg.de>
2019-06-12 17:01:11 +0200

    Added some error handling

--- src/main/java/de/uniba/dsg/jaxrs/resources/ArtistResource.java
+++ src/main/java/de/uniba/dsg/jaxrs/resources/ArtistResource.java
@@ -24,9 +24,7 @@ import de.uniba.dsg.jaxrs.exceptions.RemoteApiException;
 import de.uniba.dsg.jaxrs.exceptions.ResourceNotFoundException;
 import de.uniba.dsg.models.Song;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 
 @Path("artists")
 public class ArtistResource implements ArtistApi {
@@ -38,15 +36,12 @@ public class ArtistResource implements ArtistApi {
             throw new ClientRequestException(new ErrorMessage("Required query parameter is missing: artist-id"));
         }
         GetArtistRequest artistRequest = CustomSpotifyApi.getInstance().getArtist(artistId).build();
-
         try {
             Artist artist = artistRequest.execute();
-
             // exception: no artist found for provided artistId
             if (artist == null) {
                 throw new ResourceNotFoundException(new ErrorMessage(String.format("No artist found for query param: %s", artistId)));
             }
-
             Interpret result = new Interpret();
             result.setId(artist.getId());
             result.setName(artist.getName());
@@ -56,8 +51,12 @@ public class ArtistResource implements ArtistApi {
 
             return result;
             // exception: remote api exception
-        } catch (SpotifyWebApiException | IOException e) {
-            throw new RemoteApiException(new ErrorMessage("cannot find remote server"));
+        }
+        catch (SpotifyWebApiException | IOException e) {
+            if(e instanceof SpotifyWebApiException)
+                throw new ResourceNotFoundException(new ErrorMessage(String.format("No artist found for query param: %s", artistId)));
+            else
+                throw new RemoteApiException(new ErrorMessage("cannot find remote server"));
         }
     }
 
@@ -65,15 +64,17 @@ public class ArtistResource implements ArtistApi {
     @GET
     @Path("{artist-id}/top-tracks")
     public List<Song> getTopTracks(@PathParam("artist-id") String artistId) {
-
         if(artistId == null){
             throw new ClientRequestException(new ErrorMessage("Required query parameter is missing: artist-id"));
         }
         GetArtistsTopTracksRequest artistTopTracksRequest = CustomSpotifyApi.getInstance().getArtistsTopTracks(artistId,CountryCode.DE).build();
         try {
             Track[] tracks = artistTopTracksRequest.execute();
+            int len = tracks.length;
+            if(len > 4 )
+                len = 5;
             List<Song> songs = new ArrayList<Song>();
-            for(int i=0; i<5 ; i++) {
+            for(int i=0; i<len ; i++) {
                 Song song = new Song();
 
                 song.setArtist(tracks[i].getArtists()[0].getName());
@@ -83,8 +84,12 @@ public class ArtistResource implements ArtistApi {
                 songs.add(song);
             }
             return songs;
-        }catch (SpotifyWebApiException | IOException e) {
-            throw new RemoteApiException(new ErrorMessage("cannot find remote server"));
+        }
+        catch (SpotifyWebApiException | IOException e) {
+            if(e instanceof SpotifyWebApiException)
+                throw new ResourceNotFoundException(new ErrorMessage(String.format("No artist found for query param: %s", artistId)));
+            else
+                throw new RemoteApiException(new ErrorMessage("cannot find remote server"));
         }
     }
     
--- src/main/java/de/uniba/dsg/jaxws/resources/ArtistResource.java
+++ src/main/java/de/uniba/dsg/jaxws/resources/ArtistResource.java
@@ -17,84 +17,99 @@ import java.util.List;
 public class ArtistResource implements ArtistApi {
     @Override
     public Interpret getArtist(String artistId) {
+        try{
+            Client client = ClientBuilder.newClient();
+            Response response = client.target(MusicApiImpl.restServerUri)
+                    .path("/artists/{artist-id}")
+                    .resolveTemplate("artist-id", artistId)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get();
 
-        Client client = ClientBuilder.newClient();
-        Response response = client.target(MusicApiImpl.restServerUri)
-                .path("/artists/{artist-id}")
-                .resolveTemplate("artist-id", artistId)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get();
+            if (response.getStatus() == 200) {
+                Interpret artistInf = response.readEntity(Interpret.class);
+                return artistInf;
+            } else if (response.getStatus() == 400) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("A client side error occurred", errorMessage);
+            } else if (response.getStatus() == 404) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
+            } else if (response.getStatus()== 500) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
+            } else {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
+            }
+        } catch(Exception e) {
+            throw new MusicRecommenderFault("An internal server error occurred", e.getCause().toString());
 
-        if (response.getStatus() == 200) {
-            Interpret artistInf = response.readEntity(Interpret.class);
-            return artistInf;
-        } else if (response.getStatus() == 400) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("A client side error occurred", errorMessage);
-        } else if (response.getStatus() == 404) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
-        } else if (response.getStatus()== 500) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
-        } else {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
         }
+
     }
 
     @Override
     public List<Song> getTopTracks(String artistId) {
-        Client client = ClientBuilder.newClient();
-        Response response = client.target(MusicApiImpl.restServerUri)
-                .path("/artists/{artist-id}/top-tracks")
-                .resolveTemplate("artist-id", artistId)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get();
+        try{
+            Client client = ClientBuilder.newClient();
+            Response response = client.target(MusicApiImpl.restServerUri)
+                    .path("/artists/{artist-id}/top-tracks")
+                    .resolveTemplate("artist-id", artistId)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get();
 
-        if (response.getStatus() == 200) {
-            List<Song> songs = response.readEntity(new GenericType<List<Song>>(){});
-            return songs;
-        } else if (response.getStatus() == 400) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("A client side error occurred", errorMessage);
-        } else if (response.getStatus() == 404) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
-        } else if (response.getStatus()== 500) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
-        } else {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
+            if (response.getStatus() == 200) {
+                List<Song> songs = response.readEntity(new GenericType<List<Song>>(){});
+                return songs;
+            } else if (response.getStatus() == 400) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("A client side error occurred", errorMessage);
+            } else if (response.getStatus() == 404) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
+            } else if (response.getStatus()== 500) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
+            } else {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
+            }
+        }catch(Exception e) {
+            throw new MusicRecommenderFault("An internal server error occurred", e.getCause().toString());
+
         }
     }
     
     @Override
     public Interpret getSimilarArtist(String artistId) {
-
-        Client client = ClientBuilder.newClient();
-        Response response = client.target(MusicApiImpl.restServerUri)
-                .path("/artists/{artist-id}/similar-artist")
-                .resolveTemplate("artist-id", artistId)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get();
-
-        if (response.getStatus() == 200) {
-            Interpret artistInf = response.readEntity(Interpret.class);
-            return artistInf;
-        } else if (response.getStatus() == 400) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("A client side error occurred", errorMessage);
-        } else if (response.getStatus() == 404) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
-        } else if (response.getStatus()== 500) {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
-        } else {
-            String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
-            throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
+        try{
+            Client client = ClientBuilder.newClient();
+            Response response = client.target(MusicApiImpl.restServerUri)
+                    .path("/artists/{artist-id}/similar-artist")
+                    .resolveTemplate("artist-id", artistId)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get();
+            if (response.getStatus() == 200) {
+                Interpret artistInf = response.readEntity(Interpret.class);
+                return artistInf;
+            } else if (response.getStatus() == 400) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("A client side error occurred", errorMessage);
+            } else if (response.getStatus() == 404) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("The requested resource was not found", errorMessage);
+            } else if (response.getStatus()== 500) {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An internal server error occurred", errorMessage);
+            } else {
+                String errorMessage = response.readEntity(ErrorMessage.class).getMessage();
+                throw new MusicRecommenderFault("An unknown remote server error occurred", errorMessage);
+            }
+        }catch(Exception e) {
+            throw new MusicRecommenderFault("An internal server error occurred", e.getCause().toString());
         }
+
+
+
     }
 }
